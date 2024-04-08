5443af5f1e29ae286344439c0b26a8221e3311f1
rimma_vakhreeva <rimma2192@gmail.com>
2024-04-08 22:38:46 +0930

    Log inputs and output in subtract function in feature-z

--- calculator.py
+++ calculator.py
@@ -5,7 +5,9 @@ def add(x, y):
 
 def subtract(x, y):
     """Subtract Function"""
-    return x - y
+    result = x - y
+    print(f'Subtracting {x} from {y} gives {result}')
+    return result
 
 
 def multiply(x, y):
@@ -16,5 +18,5 @@ def multiply(x, y):
 def divide(x, y):
     """Divide Function. Raises ValueError on division by zero."""
     if y == 0:
-        raise ValueError("Can not divide by zero!")
+        raise ValueError('Can not divide by zero!')
     return x / y
